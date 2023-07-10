baeac3b14db3485e26d4bd8003811f16eab97804
Deepak Yadav <deepak@appnexio.com>
2023-07-10 20:09:29 +0530

    print and variable declaration

---
+++ day2.py
@@ -0,0 +1,58 @@
+print('deepak')
+print(12)
+print(True)
+print(False)
+print('deepak',type('deepak'))
+print(12,type(12))
+print(1.34,type(1.34))
+print(type([1,2,3])) #list []
+print(type({1,2,3})) #set {}
+print( type((1,2,3)) )#function brackets ()
+print(type({'age':35,'firstname':'Deepak'})) # dict key value
+print(type(1-3j))
+
+######################String
+firstname = 'Deepak'
+lastname = 'Yadav'
+age = 35 
+is_married = True
+address = 'Moshi Pune'
+gender='MALE'
+personal_info = {'name':'Deepak Yadav','country':'India'}
+print(firstname,lastname,age,is_married,address,len(firstname))
+
+print(firstname.upper()) 
+print(gender.lower())
+print('A in DeepAk','A' in 'DeepAk') # A
+print('A not in DeepAk','A' not in 'DeepAk') # A
+print('Find A in Deepak',firstname.find('p'))# position
+print('starts with D',firstname.startswith('D'))
+print('starts with e',firstname.startswith('e'))
+print('ends with ks',firstname.endswith('k'))
+print(firstname[0:3])
+print(firstname[3:])
+print(firstname[-2])
+print('count of e',firstname.count('e'))
+#in not is > and or < 
+print('1 is 2',1 is 2)
+print('1>2',1>2)
+print('3>1 and 2<1',3>1 and 2>1)#output
+print('3>1 or 1>1',1>=1 or 1>=1)#output
+
+
+print(2+2) #add
+print(2-3) #sub
+print(2*3) #mul
+print(2/2) #div
+print(2%2) #mod
+
+
+
+
+
+
+
+
+
+
+
